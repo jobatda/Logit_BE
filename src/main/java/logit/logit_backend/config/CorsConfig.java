3b1804b43ea3e3b8c981e79a45c1e1a2f14fefdf
nuyheat <taehkim2@student.42seoul.kr>
2025-02-12 20:17:38 +0900

    fix: cors error

---
+++ src/main/java/logit/logit_backend/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package logit.logit_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // 모든 엔드포인트 허용
+                        .allowedOrigins("*") // 모든 Origin 허용
+                        .allowedMethods("*") // 모든 HTTP 메서드 허용
+                        .allowedHeaders("*") // 모든 헤더 허용
+                        .allowCredentials(false); // 인증 정보 포함 여부 (true면 보안 이슈 발생 가능)
+            }
+        };
+    }
+}
