ba259b508e44bf098de0239ba9fa6896c7e6dff4
nuyheat <taehkim2@student.42seoul.kr>
2025-02-12 15:32:55 +0900

    refactor: change Controller to RestController

--- src/main/java/logit/logit_backend/controller/CourseController.java
+++ src/main/java/logit/logit_backend/controller/CourseController.java
@@ -26,7 +26,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 
-@Controller
+@RestController
 @RequestMapping("/course")
 public class CourseController {
     private final CourseService courseService;
