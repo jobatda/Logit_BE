bda6c839bc39c79227d78fb10fd7a263096a11c7
nuyheat <taehkim2@student.42seoul.kr>
2025-02-14 07:42:58 +0900

    fix: logger error

--- src/main/java/logit/logit_backend/controller/CourseController.java
+++ src/main/java/logit/logit_backend/controller/CourseController.java
@@ -25,7 +25,6 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
-import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/course")
