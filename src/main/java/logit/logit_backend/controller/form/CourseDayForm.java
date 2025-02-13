71d8c54fe0b528f80116b3b71953d422c581f351
nuyheat <taehkim2@student.42seoul.kr>
2025-02-14 07:53:15 +0900

    feat: change filename

--- src/main/java/logit/logit_backend/controller/form/CourseDayForm.java
+++ src/main/java/logit/logit_backend/controller/form/CourseDayForm.java
@@ -7,16 +7,9 @@ import lombok.Setter;
 @Getter @Setter
 public class CourseDayForm {
 
-    @JsonProperty("카테고리")
-    private String category;
-
-    @JsonProperty("장소명")
-    private String placeName;
-
-    @JsonProperty("주소")
-    private String address;
-
-    @JsonProperty("imgUrl")
+    private String 카테고리;
+    private String 장소명;
+    private String 주소;
     private String imgUrl; // null 가능
 
 }
